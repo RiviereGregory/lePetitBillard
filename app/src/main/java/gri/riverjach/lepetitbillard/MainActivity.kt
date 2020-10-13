4f8713158b42769fe6bf68163f12495a237ed019
RIVIERE <gregory.riviere@laposte.net>
2020-10-13 22:00:27 +0200

    Mon premier bouton

--- app/src/main/java/gri/riverjach/lepetitbillard/MainActivity.kt
+++ app/src/main/java/gri/riverjach/lepetitbillard/MainActivity.kt
@@ -1,11 +1,19 @@
 package gri.riverjach.lepetitbillard
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onClick(v: View) {
+        val x = 10
+        if (x >= 10)
+            Toast.makeText(this, "Je presse mon premier bouton", Toast.LENGTH_LONG).show()
     }
 }
