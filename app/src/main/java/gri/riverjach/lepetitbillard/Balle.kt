30aa7b8b4f50fecb875eda934ef0a2ba0e61eb21
RIVIERE <gregory.riviere@laposte.net>
2020-10-21 19:55:08 +0200

    Ajout Reagir au click - Affiche le diametre des balles

--- app/src/main/java/gri/riverjach/lepetitbillard/Balle.kt
+++ app/src/main/java/gri/riverjach/lepetitbillard/Balle.kt
@@ -12,14 +12,20 @@ class Balle(x1: Float, y1: Float, val diametre: Float) {
     val paint = Paint()
     val r = RectF(x1, y1, x1 + diametre, y1 + diametre)
     var color = Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256))
+    var showText = false
 
 
     fun draw(canvas: Canvas?) {
         paint.color = color
         canvas?.drawOval(r, paint)
+        //canvas?.drawText("x ${this.r.centerX()}  y ${this.r.centerY()}", r.right, r.top, paint)
+        if (showText) {
+            paint.textSize = 30f
+            canvas?.drawText("mon diamètre est $diametre", r.left, r.top, paint)
+        }
     }
 
-    fun changeCouleur(){
+    fun changeCouleur() {
         color = Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256))
     }
 
--- app/src/main/java/gri/riverjach/lepetitbillard/DrawingView.kt
+++ app/src/main/java/gri/riverjach/lepetitbillard/DrawingView.kt
@@ -5,6 +5,7 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.SurfaceView
 import java.util.*
 
@@ -36,5 +37,18 @@ class DrawingView @JvmOverloads constructor(
         balle1.changeCouleur()
         balle2.changeCouleur()
         balle3.changeCouleur()
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        if(event.action == MotionEvent.ACTION_DOWN){
+            val x = event.rawX
+            val y = event.rawY - 260
+            //Toast.makeText(context, "x $x y $y", Toast.LENGTH_SHORT).show()
+            if (balle1.r.contains(x,y)) balle1.showText = !balle1.showText
+            if (balle2.r.contains(x,y)) balle2.showText = !balle2.showText
+            if (balle3.r.contains(x,y)) balle3.showText = !balle3.showText
+        }
+        invalidate()
+        return true
     }
 }
